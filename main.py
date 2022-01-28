2ad8fe00d50556fac2f4cddb48f99986f3c5dd71
shyam <shyamnalluri96@gmail.com>
2022-01-28 14:39:30 +0000

    Initial commit

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ import pyttsx3
 import pywhatkit
 import wikipedia
 from datetime import datetime
-import pyjokes
 
 def talk(answer):
     engine = pyttsx3.init()
@@ -15,27 +14,42 @@ def talk(answer):
     engine.say(answer)
     engine.runAndWait()
 
-def processquestion(question):
+r = sr.Recognizer()
+
+with sr.Microphone() as source:
+    answer = 'You are speaking to AI voice assistant, How can I help!'
+    print('Voice Assistant:', answer)
+    talk(answer)
+    print("listening....")
+    audio = r.listen(source)
+
+try:
+    #print(r.recognize_google(audio))
+    question = r.recognize_google(audio)
+    if 'Alexa' in question:
+        question = question.replace('Alexa','')
+        #print(question)
+
 
     if 'what are you doing' in question:
         print("Human:", question)
         answer = ('I am waiting for your question')
         print('Voice Assistant:', answer)
         talk(answer)
-        return True
+
 
     elif 'how are you' in question:
         print("Human:", question)
         answer = ('I am doing good, how are you dear')
         talk(answer)
         print('Voice Assistant:', answer)
-        return True
+
 
     elif 'play' in question:
         print("Human:", question)
         question = question.replace('play', '')
         pywhatkit.playonyt(question)
-        return True
+
 
     elif 'tell me about' in question:
         print("Human:", question)
@@ -43,41 +57,40 @@ def processquestion(question):
         wiki = wikipedia.summary(question, 1)
         print(wiki)
         talk(wiki)
-        return True
+
 
     elif 'time' in question:
         print("Human:", question)
         time = datetime.today().time().strftime('%I:%M %p')
         print(time)
         talk(time)
-        return True
+
 
     elif 'joke' in question:
         print("Human:", question)
         joke = pyjokes.get_joke()
         print(pyjokes.get_joke())
-        return True
+
 
     elif 'age' in question:
         print("Human:", question)
         question = question.replace('age', '')
         answer = ('Why do you want my age fuckig bitch')
         talk(answer)
-        return True
+
 
     elif 'what does' in question:
         print("Human:", question)
         question = question.replace('what does', '')
         answer = ('nani')
         talk(answer)
-        return True
 
     elif 'weather' in question:
         print("Human:", question)
         question = question.replace('weather', '')
         answer = get_weather('london')
         talk(answer)
-        return True
+
 
     else:
         question = r.recognize_google(audio)
@@ -85,42 +98,9 @@ def processquestion(question):
         answer = ("sorry,I can't find answer to", question, "Good Bye")
         print("Good Bye")
         talk(answer)
-        return False
-
-def getquestion():
-    r = sr.Recognizer()
-
-    with sr.Microphone() as source:
-        answer = 'You are speaking to AI voice assistant, How can I help!'
-        print('Voice Assistant:', answer)
-        talk(answer)
-        print("listening....")
-        audio = r.listen(source)
-
-    try:
-        #print(r.recognize_google(audio))
-        question = r.recognize_google(audio)
-        if 'Alexa' in question:
-            question = question.replace('Alexa','')
-            #print(question)
-            return question
-        else:
-            print("You are not talking to me")
-            return "notwithme"
 
 
-    except sr.UnknownValueError:
-        answer = "Since No response from you, Good bye for now"
-        print("Since No Response from you, Good bye for now")
-        talk(answer)
-        return False
-
-canAskQuestion = True
-while canAskQuestion:
-    question = getquestion()
-    if (question == "notwithme"):
-        talk("Ok,carry on with your friends")
-        print("Ok,carry on with your friends")
-        canaskquestion = False
-    else:
-        processquestion(question)
+except sr.UnknownValueError:
+    answer = "Since No response from you, Good bye for now"
+    print("Since No Response from you, Good bye for now")
+    talk(answer)
